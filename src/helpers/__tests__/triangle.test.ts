d0d1038ae8ac0bd53a8bfb4fe61603d1f2f132f3
John Slipper <john.slipper@noted.com>
2022-02-18 14:22:00 +1300

    Add triangle type function tests

--- src/helpers/__tests__/triangle.test.ts
+++ src/helpers/__tests__/triangle.test.ts
@@ -5,4 +5,16 @@ describe("test triangle type function", () => {
     const result = getTypeOfTriangle(5, 5, 5);
     expect(result).toBe(TriangleType.Equilateral);
   });
+  test("it is scalene", () => {
+    const result = getTypeOfTriangle(1, 2, 3);
+    expect(result).toBe(TriangleType.Scalene);
+  });
+  test("it is isosceles", () => {
+    const result = getTypeOfTriangle(1, 1, 3);
+    expect(result).toBe(TriangleType.Isosceles);
+  });
+  test("it is unknown", () => {
+    const result = getTypeOfTriangle(-1, 1, 3);
+    expect(result).toBe(TriangleType.Unknown);
+  });
 });
